8735f325dcb84f4a7146fb83788eac6afb8ec583
Humberto Atondo <a00959979@itesm.mx>
2017-08-22 19:14:04 -0500

    [A00959979-Homework-01]
    Signed-off-by: Humberto Atondo <a00959979@itesm.mx>

--- labs/01/hello.c
+++ labs/01/hello.c
@@ -4,6 +4,6 @@
 
 int main(void) {
     printf("Hello World\n");
-    printf("My name is Christopher Salvador Márquez Álvarez \n");
+    printf("My name is Humberto Atondo Martin del Campo \n");
     return 0; 
 }
